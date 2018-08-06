15c11a6069d4e59d484b098f8a1cb6b109df7bc6
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-08-06 08:40:17 +0700

    updated new version'

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -24,7 +24,7 @@ export default class Download extends React.Component {
           <Box align="center" direction="row">
             <Button
               primary={true}
-              href="https://github.com/ZeroX-DG/SnippetStore/releases/download/v0.2.4/Snippet.Store.Setup.0.2.4.exe"
+              href="https://github.com/ZeroX-DG/SnippetStore/releases/download/v0.2.5/Snippet.Store.Setup.0.2.5.exe"
               className="download-btn"
               pad="large"
               label={
@@ -37,7 +37,7 @@ export default class Download extends React.Component {
               className="download-btn"
               primary={true}
               label="Linux / 64bit"
-              href="https://github.com/ZeroX-DG/SnippetStore/releases/download/v0.2.4/snippetstore_0.2.4_amd64.deb"
+              href="https://github.com/ZeroX-DG/SnippetStore/releases/download/v0.2.5/snippetstore_0.2.5_amd64.deb"
               label={
                 <div>
                   <i className="fa fa-linux" />&nbsp;Linux / 64bit (.deb)
@@ -48,7 +48,7 @@ export default class Download extends React.Component {
               className="download-btn"
               primary={true}
               label="Linux / 64bit"
-              href="https://github.com/ZeroX-DG/SnippetStore/releases/download/v0.2.4/snippetstore-0.2.4-x86_64.AppImage"
+              href="https://github.com/ZeroX-DG/SnippetStore/releases/download/v0.2.5/snippetstore-0.2.5-x86_64.AppImage"
               label={
                 <div>
                   <i className="fa fa-linux" />&nbsp;Linux / 64bit (.AppImage)
