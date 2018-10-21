22a496a48f61ffe3b4c880db950198b6b9a0a959
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-10-21 10:31:47 +0700

    updated version

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Hero, Paragraph, Box, Heading, Image } from "grommet";
-import axios from "axios";
 
 export default class Intro extends React.Component {
   render() {
@@ -17,7 +16,7 @@ export default class Intro extends React.Component {
                 A snippet management app for developers
               </Paragraph>
               <Paragraph size="large">
-                v0.2.8 -{" "}
+                v0.2.9 -{" "}
                 <a href="https://github.com/ZeroX-DG/SnippetStore/blob/master/CHANGELOG.md">
                   Changelog
                 </a>
